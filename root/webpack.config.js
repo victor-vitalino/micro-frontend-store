67f3205e2632f0905697e603775dd5a423da5e1a
Victor Vitalino <victorvitalino237@gmail.com>
2024-09-19 00:29:54 -0300

    :bug: fix(ROOT): fix organization name

--- root/webpack.config.js
+++ root/webpack.config.js
@@ -3,7 +3,7 @@ const singleSpaDefaults = require("webpack-config-single-spa-ts");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = (webpackConfigEnv, argv) => {
-  const orgName = "root";
+  const orgName = "store";
   const defaultConfig = singleSpaDefaults({
     orgName,
     projectName: "root-config",
